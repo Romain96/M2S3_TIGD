0aae8f70bd7af7e4e594de1b47c6835c33430da3
Romain PERRIN <romain.perrin@etu.unistra.fr>
2019-01-08 16:44:06 +0100

    adding Pixel class code

---
+++ src/Pixel.hxx
@@ -0,0 +1,55 @@
+#ifndef __PIXEL_HXX__
+#define __PIXEL_HXX__
+
+#include "../include/Pixel.h"
+
+// constructor
+Pixel::Pixel() :
+    _x(0),
+    _y(0),
+    _value(0)
+{
+    // nothing
+}
+
+Pixel::Pixel(unsigned char x, unsigned char y, unsigned char value) :
+    _x(x),
+    _y(y),
+    _value(value)
+{
+    // nothing
+}
+
+// getters
+unsigned char Pixel::getX()
+{
+    return _x;
+}
+
+unsigned char Pixel::getY()
+{
+    return _y;
+}
+
+unsigned char Pixel::getValue()
+{
+    return _value;
+}
+
+// setters
+void Pixel::setX(unsigned char x)
+{
+    _x = x;
+}
+
+void Pixel::setY(unsigned char y)
+{
+    _y = y;
+}
+
+void Pixel::setValue(unsigned char value)
+{
+    _value = value;
+}
+
+#endif
