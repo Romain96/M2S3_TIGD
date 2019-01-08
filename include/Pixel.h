dec89005e8196f055d3caa5457c42dec9fd3863b
Romain PERRIN <romain.perrin@etu.unistra.fr>
2019-01-08 16:43:45 +0100

    adding Pixel class

---
+++ include/Pixel.h
@@ -0,0 +1,39 @@
+#ifndef __PIXEL_H__
+#define __PIXEL_H__
+
+/*
+ * TIDG : mathematical morphology
+ * Romain Perrin <romain.perrin@etu.unistra.fr>
+ * Maxime Seyer <maxime.seyer@etu.unistra.fr>
+ * Université de Strasbourg
+ * 2018-2019
+ */
+
+// pixel class : contains pixel coordinates X,Y and its intensity [0-255]
+class Pixel
+{
+    // attributes
+protected:
+    unsigned char _x;
+    unsigned char _y;
+    unsigned char _value;
+
+    // methods
+public:
+    Pixel();
+    Pixel(unsigned char x, unsigned char y, unsigned char value);
+
+    // getters
+    unsigned char getX();
+    unsigned char getY();
+    unsigned char getValue();
+
+    // setters
+    void setX(unsigned char x);
+    void setY(unsigned char y);
+    void setValue(unsigned char value);
+};
+
+#include "../src/Pixel.hxx"
+
+#endif
