491bc1d101db98f8df3306251cf71af32e6596da
Romain PERRIN <romain.perrin@etu.unistra.fr>
2019-01-14 15:19:04 +0100

    adding main + two binary PGM files to test

--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 #include <vector>
 #include <iostream>
 #include "include/Pixel.h"
+#include "include/CTree.h"
+
+#include "libtim/Common/Types.h"
+#include "libtim/Common/Image.h"
+
+using namespace LibTIM;
 
 int main()
 {
-	UFDSet set(16, 4);	// 16 pixels aka 4x4 image
+	/*UFDSet set(16, 4);	// 16 pixels aka 4x4 image
 	for (int i = 0; i < 4; i++)
 	{
 		for (int j = 0; j < 4; j++)
@@ -18,7 +24,13 @@ int main()
 
 	set.link(Pixel(0,0,255), Pixel(1,1,255));
 	set.link(Pixel(0,0,255), Pixel(2,2,255));
-	set.selfDisplay();
+	set.selfDisplay();*/
+
+	Image<U8> img;
+	img.load("lena.pgm", img);
+
+	CTree ct(img);
+	ct.buildComponentTree();
 
 	return 0;
 }
